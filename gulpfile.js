12a3c15d457212cd9d19554e4fe4ada627fec52f
Doni Setiawan <donisetiawan.050@gmail.com>
2020-04-06 20:50:04 +0700

    Creating React Elements with JSX

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,8 @@ const browserify = require('browserify');
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 
-gulp.task('default', function () {
-  return browserify('./source/app.js')
-    .transform(babelify)
-    .bundle()
-    .pipe(source('snapterest.js'))
-    .pipe(gulp.dest('./build/'));
-});
+gulp.task('default', () => browserify('./source/app.jsx')
+  .transform(babelify)
+  .bundle()
+  .pipe(source('snapterest.js'))
+  .pipe(gulp.dest('./build/')));
